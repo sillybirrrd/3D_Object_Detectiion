e2a1802398ac65674ae599efabbe3742a589fd4e
sypj <71678608+sillybirrrd@users.noreply.github.com>
2022-06-15 21:48:43 +0800

    Update kitti_download.sh

--- kitti_download.sh
+++ kitti_download.sh
@@ -15,7 +15,7 @@ unzip -d ./data/kitti ./data_object_label_2.zip > /dev/null
 
 rm ./data_object_velodyne.zip ./data_object_image_2.zip ./data_object_calib.zip ./data_object_label_2.zip ./devkit_object.zip
 
-# Download data split
+
 wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/test.txt --no-check-certificate --content-disposition -O ./data/kitti/ImageSets/test.txt
 wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/train.txt --no-check-certificate --content-disposition -O ./data/kitti/ImageSets/train.txt
 wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/val.txt --no-check-certificate --content-disposition -O ./data/kitti/ImageSets/val.txt
