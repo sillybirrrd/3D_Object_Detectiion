c84233963a498815e7bd67143f56375cf8b79afc
sypj <71678608+sillybirrrd@users.noreply.github.com>
2022-07-02 21:56:41 +0800

    Update kitti_download.sh

--- kitti_download.sh
+++ kitti_download.sh
@@ -2,19 +2,22 @@
 
 cd ./mmdetection3d
 mkdir ./data/kitti/ && mkdir ./data/kitti/ImageSets
+
 wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_velodyne.zip
-wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_image_2.zip
-wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_calib.zip
-wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_label_2.zip
-
-
 unzip -d ./data/kitti ./data_object_velodyne.zip > /dev/null
+rm ./data_object_velodyne.zip
+
+wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_image_2.zip
 unzip -d ./data/kitti ./data_object_image_2.zip > /dev/null
+rm  ./data_object_image_2.zip 
+
+wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_calib.zip
 unzip -d ./data/kitti ./data_object_calib.zip > /dev/null
+rm ./data_object_calib.zip 
+
+wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_label_2.zip
 unzip -d ./data/kitti ./data_object_label_2.zip > /dev/null
-
-rm ./data_object_velodyne.zip ./data_object_image_2.zip ./data_object_calib.zip ./data_object_label_2.zip ./devkit_object.zip
-
+rm ./data_object_label_2.zip ./devkit_object.zip
 
 wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/test.txt --no-check-certificate --content-disposition -O ./data/kitti/ImageSets/test.txt
 wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/train.txt --no-check-certificate --content-disposition -O ./data/kitti/ImageSets/train.txt
